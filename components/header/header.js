bd62726e605fb34e8611e5b8f17fa3d75eacf34f
Alexandre <lelex.g@hotmail.com>
2021-06-11 16:19:39 -0300

    add - title

--- components/header/header.js
+++ components/header/header.js
@@ -30,6 +30,7 @@ const Header = () => {
   return (
     <div class="header" id="myHeader">
       <Head>
+        <title>Volun</title>
         <meta charset="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
