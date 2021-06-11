8777080d9356af45f23a091c0dd98a11d6196e76
Alexandre <lelex.g@hotmail.com>
2021-06-11 18:58:49 -0300

    fix ong

--- pages/ong/index.js
+++ pages/ong/index.js
@@ -20,9 +20,7 @@ const GET_PROJECTS = gql`
       workload
       Organization {
         id
-        photos {
-          description
-        }
+        photo
       }
       Photo {
         description
