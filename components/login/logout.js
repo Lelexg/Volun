51108d5bab2a8fd5098d571469778dbffbe7569b
Alexandre <lelex.g@hotmail.com>
2021-06-11 17:06:54 -0300

    fix logout

--- components/login/logout.js
+++ components/login/logout.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LogoutButton = () => {
   const { logout, LogoutOptions } = useAuth0();
 
-  return <a className="logout" onClick={() => logout({redirectTo: 'http://localhost:3000'})}>Sair</a>;
+  return <a className="logout" onClick={() => logout({redirectTo: 'https://volun-portal.vercel.app'})}>Sair</a>;
 };
 
 export default LogoutButton;
